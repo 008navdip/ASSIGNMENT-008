81565c397223e47ff4e0f6813a851e7d0a958a76
HP <HP@LAPTOP-L437ARAR>
2020-11-12 09:45:12 +1300

    Add project files.

---
+++ ExpensesTableApi/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpensesTableApi.Models
+{
+    public class Product // product model class which is bought and added into expenses
+    {
+        // this is primary key
+        [Key]// data annotations
+        public int Id { get; set; }
+        // prduct name
+        public string Name { get; set; }
+        // quantity bought
+        public int Quantity { get; set; }
+        // price each
+        public double  Price { get; set; }
+    }
+}
